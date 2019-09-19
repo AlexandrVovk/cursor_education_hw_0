b40f6ddb819b260547287312e83caf963c18c8c5
Alexandr.Vovk.V <Alexandr.Vovk.V@gmail.com>
2019-09-20 00:57:31 +0300

    update

--- sort.py
+++ sort.py
@@ -2,11 +2,16 @@
 import os
 import sys
 
+
 digit_one = raw_input("please type first roman digit: ")
+
+digit_one = digit_one.upper()
 
 #print (digit_one)
 
 digit_two = raw_input("please type first roman digit: ")
+
+digit_two = digit_two.upper()
 
 #print (digit_two)
 
@@ -14,16 +19,21 @@ sum = digit_one+digit_two
 
 #print(sum)
 
+#####Sort sum value by numeric from list####
 list = {'M': '1', 'D': '2', 'C': '3', 'L': '4', 'X': '5', 'V': '6', 'I': '7'}
 
 result_list = sorted(sum, key=list.__getitem__)
 
+#print (result_list)
+
+#####Move value from LIST to STR####
 result_str = ''.join(result_list)
 
-print(result_str)
+#print(result_str)
 
+#####Calculate digit by REPLACE function#####
 result = result_str.replace('IIIII','V').replace('VV','X').replace('XXXXX','L').replace('LL','C').replace('CCCCC','D').replace('DD','M')
 
-print (result)
+#print (result)
 
 print 'The result is: ', result
